57af0a3cf2a6642fcd641e3417b3cc2d8e498529
AtanasG6 <gyulchev666@gmail.com>
2024-04-10 12:22:10 +0300

    First task

--- Exercise 1/Exercise1/Program.cs
+++ Exercise 1/Exercise1/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! 2");
+            //Console.WriteLine("Hello, World! 2");
+            int num = readNumber();
+            Console.WriteLine($"The sum until {num} is {findSumTillN(num)}.");
+        }
+
+        static int readNumber()
+        {
+            int num;
+            Console.Write("Enter a whole positive (>0) number: ");
+            do
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num<=0)
+                {
+                    Console.Write("Invalid num! Try again: ");
+                }
+            }while (num<=0);
+
+            return num;
+        }
+
+        static int findSumTillN(int n)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
         }
     }
 }
