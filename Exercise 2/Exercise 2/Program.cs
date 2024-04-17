24bb8cd98fe9d9dedcddbd4a5fbb857dabbd3e4e
AtanasG6 <gyulchev666@gmail.com>
2024-04-17 12:21:31 +0300

    Second problem

--- Exercise 2/Exercise 2/Program.cs
+++ Exercise 2/Exercise 2/Program.cs
@@ -3,12 +3,20 @@
 //Swap(ref a, ref b);
 //Console.WriteLine($"a = {a}; b = {b}");
 
-int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+//int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-int min, avg, max;
-ArrStats(arr, out min, out avg, out max);
+//int min, avg, max;
+//ArrStats(arr, out min, out avg, out max);
 
-Console.WriteLine($"Min = {min}. Avg = {avg}. Max = {max}.");
+//Console.WriteLine($"Min = {min}. Avg = {avg}. Max = {max}.");
+bool isEven, isOdd;
+int num1 = 10,
+    num2 = 11;
+
+isEvenOrOdd(num1, out isEven, out isOdd);
+Console.WriteLine($"num1 = {num1}: isEven = {isEven}, isOdd = {isOdd}.");
+isEvenOrOdd(num2, out isEven, out isOdd);
+Console.WriteLine($"num1 = {num2}: isEven = {isEven}, isOdd = {isOdd}.");
 
 static void Swap(ref int a, ref int b)
 {
@@ -22,4 +30,18 @@ static void ArrStats(int[] arr, out int min, out int avg, out int max)
     min = arr.Min();
     max = arr.Max();
     avg = (int)arr.Average();
+}
+
+static void isEvenOrOdd(int num, out bool isEven, out bool isOdd)
+{
+    if (num % 2 == 0)
+    {
+        isEven = true;
+        isOdd = false;
+    }
+    else
+    {
+        isEven = false;
+        isOdd = true;
+    }
 }
