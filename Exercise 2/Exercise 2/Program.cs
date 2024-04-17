428a89043a2034b2d9e9388e2350325f1dd21cc9
AtanasG6 <gyulchev666@gmail.com>
2024-04-17 13:53:31 +0300

    New problems solved

--- Exercise 2/Exercise 2/Program.cs
+++ Exercise 2/Exercise 2/Program.cs
@@ -9,14 +9,21 @@
 //ArrStats(arr, out min, out avg, out max);
 
 //Console.WriteLine($"Min = {min}. Avg = {avg}. Max = {max}.");
-bool isEven, isOdd;
-int num1 = 10,
-    num2 = 11;
+//bool isEven, isOdd;
+//int num1 = 10,
+//    num2 = 11;
 
-isEvenOrOdd(num1, out isEven, out isOdd);
-Console.WriteLine($"num1 = {num1}: isEven = {isEven}, isOdd = {isOdd}.");
-isEvenOrOdd(num2, out isEven, out isOdd);
-Console.WriteLine($"num1 = {num2}: isEven = {isEven}, isOdd = {isOdd}.");
+//isEvenOrOdd(num1, out isEven, out isOdd);
+//Console.WriteLine($"num1 = {num1}: isEven = {isEven}, isOdd = {isOdd}.");
+//isEvenOrOdd(num2, out isEven, out isOdd);
+//Console.WriteLine($"num1 = {num2}: isEven = {isEven}, isOdd = {isOdd}.");
+
+Profile newProfile = new Profile("New Language", "New Theme", "New Font");
+Profile profile = new Profile("Default Language", "Default Theme", "Default Font");
+
+UpdateProfileSettings(profile, newProfile);
+
+Console.WriteLine($"{profile.Language}, {profile.Theme}, {profile.Font}");
 
 static void Swap(ref int a, ref int b)
 {
@@ -45,3 +52,25 @@ static void isEvenOrOdd(int num, out bool isEven, out bool isOdd)
         isOdd = true;
     }
 }
+
+static void UpdateProfileSettings(Profile oldProfile, Profile newProfile)
+{
+    oldProfile.Language = newProfile.Language;
+    oldProfile.Theme = newProfile.Theme;
+    oldProfile.Font = newProfile.Font;
+}
+
+class Profile
+{
+    public string Language { get; set; }
+    public string Theme { get; set; }
+    public string Font { get; set; }
+
+    public Profile(string language, string theme, string font)
+    {
+        Language = language;
+        Theme = theme; 
+        Font = font;
+    }
+}
+
