6a1396be127a1f87fea63b0846af55ffe0a35dea
AtanasG6 <gyulchev666@gmail.com>
2024-04-24 12:56:01 +0300

    Task 2

---
+++ Exercise 3/Exercise 3/MyRectangle.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercise_3
+{
+    public class MyRectangle
+    {
+        public Point PointA { get; set; }
+        public Point PointB { get; set; }
+
+        public MyRectangle(Point pointA, Point pointB)
+        {
+            PointA = pointA;
+
+            PointB = pointB;
+        }
+
+        public double Perimeter()
+        {
+            double dx = Math.Abs(PointA.X - PointB.X);
+            double dy = Math.Abs(PointA.Y - PointB.Y);
+            double perimeter = 2 * (dx + dy);
+            return Math.Round(perimeter, 2);
+        }
+
+        public double Face()
+        {
+            double dx = Math.Abs(PointA.X - PointB.X);
+            double dy = Math.Abs(PointA.Y - PointB.Y);
+
+            double face = dx * dy;
+            return Math.Round(face, 2);
+        }
+    }
+}
--- Exercise 3/Exercise 3/Program.cs
+++ Exercise 3/Exercise 3/Program.cs
@@ -6,18 +6,23 @@ class Program
 {
     public static void Main()
     {
-        var product1 = new Product(1, 10, DateTime.Now.AddDays(3), "Product1");
-        var product2 = new Product(2, 12, DateTime.Now.AddDays(4), "Product2");
+        //var product1 = new Product(1, 10, DateTime.Now.AddDays(3), "Product1");
+        //var product2 = new Product(2, 12, DateTime.Now.AddDays(4), "Product2");
 
-        var cart = new Cart();
-        cart.Products.Add(product1);
-        cart.Products.Add(product2);
-        Console.WriteLine(cart.TotalSum());
+        //var cart = new Cart();
+        //cart.Products.Add(product1);
+        //cart.Products.Add(product2);
+        //Console.WriteLine(cart.TotalSum());
 
-        var store = new Store();
-        store.AddAmount(product1, 12);
-        var searchedProduct = store.Find("Product1");
-        Console.WriteLine(searchedProduct.Quantity);
+        //var store = new Store();
+        //store.AddAmount(product1, 12);
+        //var searchedProduct = store.Find("Product1");
+        //Console.WriteLine(searchedProduct.Quantity);
 
+
+        MyRectangle rect1 = new MyRectangle(new Point(3.3, 6.7), new Point(4.5, 9));
+
+        Console.WriteLine("Perimeter: " + rect1.Perimeter());
+        Console.WriteLine("Area " + rect1.Face());
     }
 }
