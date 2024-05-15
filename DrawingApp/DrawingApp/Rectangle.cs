599a579d9bc65c840b71fb9f9ea153c40b76889b
AtanasG6 <gyulchev666@gmail.com>
2024-05-15 13:54:44 +0300

    Drawing app

---
+++ DrawingApp/DrawingApp/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿namespace DrawingApp
+{
+    public class Rectangle : Shape
+    {
+        private int x, y, width, height;
+        public Rectangle(Pen pen, int x, int y, int width, int height)
+        {
+            this.pen = pen;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+        public override void Draw(Graphics g)
+        {
+            g.DrawRectangle(pen, x, y, width, height);
+        }
+    }
+}
